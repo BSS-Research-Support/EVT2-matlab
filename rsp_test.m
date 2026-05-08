dfdc7818efcf67a390862c96430d60e4b070da15
M. Stokroos <44379672+MartinStokroos@users.noreply.github.com>
2026-05-08 11:01:29 +0200

    verbose read

--- rsp_test.m
+++ rsp_test.m
@@ -2,7 +2,7 @@
 evt2('open', 0x0408, 0x005); % use USB VID, PID
 
 for n = 1:10
-    evt2('read'); % function is blocking with 1s timeout.
+    evt2('read') % function is blocking with 1s timeout.
 end
 
 evt2('close');
